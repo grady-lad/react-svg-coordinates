461fda606194c4e59c1d51a58748b0ffce094d83
grady-lad <ogradymartin7@gmail.com>
2017-12-10 09:53:45 -0600

    Setting up boilerplate

---
+++ README.md
@@ -0,0 +1,2 @@
+# react-svg-coordfuncs
+
---
+++ src/index.js
@@ -0,0 +1,20 @@
+/**
+ * @class ExampleComponent
+ */
+
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+import "./styles.css";
+
+export default class ExampleComponent extends Component {
+  static propTypes = {
+    text: PropTypes.string
+  };
+
+  render() {
+    const { text } = this.props;
+
+    return <div className="test">Example Component: {text}</div>;
+  }
+}
