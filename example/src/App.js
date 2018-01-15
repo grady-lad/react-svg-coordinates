81b8b4f3fdc18ce3b66780870fcba06a53b51431
Stephanie Tassone <stephanie.tassone@gmail.com>
2018-01-15 22:20:05 +1100

    Update page styles

--- example/src/App.js
+++ example/src/App.js
@@ -2,24 +2,30 @@ import React from 'react';
 import { LineChart, AreaChart, ScatterPlot } from './Charts/';
 
 const App = () => (
-  <div className="container-ex" style={{ textAlign: 'center' }}>
-    <h1>React SVG Coordiante Functions</h1>
-    <p> A lib that helps you map your dataset on an svg</p>
-    <section>
-      <h2>Line Chart</h2>
-      <LineChart />
-      <div />
-    </section>
-    <section>
-      <h2>Area Chart</h2>
-      <div>
-        <AreaChart />
-      </div>
-    </section>
-    <section>
-      <h2>Scatter Plot</h2>
-      <div> View source enter github url here</div>
-    </section>
+  <div>
+    <div className="header-container">
+      <h1 className="header">React SVG Coordinate Functions</h1>
+      <p className="sub-header">
+        A library that helps you map your data set on an svg
+      </p>
+    </div>
+    <div className="container-ex" style={{ textAlign: 'center' }}>
+      <section>
+        <h2>Line Chart</h2>
+        <LineChart />
+        <div />
+      </section>
+      <section>
+        <h2>Area Chart</h2>
+        <div>
+          <AreaChart />
+        </div>
+      </section>
+      <section>
+        <h2>Scatter Plot</h2>
+        <div> View source enter github url here</div>
+      </section>
+    </div>
   </div>
 );
 
